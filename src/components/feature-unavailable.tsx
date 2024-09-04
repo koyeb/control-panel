0f979c827a656f6e234047d345e77c96ca493891
nils <nilscox.dev@gmail.com>
2024-09-04 13:04:31 +0200

    create a component for when a feature is unavailable

---
+++ src/components/feature-unavailable.tsx
@@ -0,0 +1,42 @@
+import { BadgeNew } from 'src/components/badge-new';
+import { Title } from 'src/components/title';
+import { Translate } from 'src/intl/translate';
+
+const T = Translate.prefix('featureUnavailable');
+
+type FeatureUnavailableProps = {
+  preview?: 'technical' | 'public';
+  title: React.ReactNode;
+  subTitle: React.ReactNode;
+  description: React.ReactNode;
+  cta: React.ReactNode;
+  documentationLink?: React.ReactNode;
+};
+
+export function FeatureUnavailable({
+  preview,
+  title,
+  subTitle,
+  description,
+  cta,
+  documentationLink,
+}: FeatureUnavailableProps) {
+  return (
+    <>
+      {preview !== undefined && (
+        <BadgeNew className="mb-1">{preview === 'technical' && <T id={`${preview}Preview`} />}</BadgeNew>
+      )}
+
+      <Title title={title} />
+
+      <p className="mb-4 mt-2 font-medium">{subTitle}</p>
+
+      <p className="max-w-xl">{description}</p>
+
+      <div className="row mt-6 items-center gap-4">
+        {cta}
+        {documentationLink}
+      </div>
+    </>
+  );
+}
--- src/pages/domains/components/domains-locked.tsx
+++ src/pages/domains/components/domains-locked.tsx
@@ -1,35 +1,29 @@
 import { routes } from 'src/application/routes';
 import { DocumentationLink } from 'src/components/documentation-link';
+import { FeatureUnavailable } from 'src/components/feature-unavailable';
 import { IconArrowRight } from 'src/components/icons';
 import { LinkButton } from 'src/components/link';
-import { Title } from 'src/components/title';
 import { Translate } from 'src/intl/translate';
 
 const T = Translate.prefix('pages.domains.domainsLocked');
 
-export const DomainsLocked = () => {
+export function DomainsLocked() {
   return (
-    <div className="col items-start gap-4 rounded-md border p-6">
-      <div className="col gap-2">
-        <Title title={<T id="title" />} />
-
-        <p className="font-medium">
-          <T id="description" />
-        </p>
-      </div>
-
-      <p>
-        <T id="line1" values={{ br: () => <br /> }} />
-      </p>
-
-      <DocumentationLink path="/docs/run-and-scale/domains">
-        <T id="docs" />
-      </DocumentationLink>
-
-      <LinkButton href={routes.organizationSettings.plans()}>
-        <T id="upgrade" />
-        <IconArrowRight className="size-icon" />
-      </LinkButton>
-    </div>
+    <FeatureUnavailable
+      title={<T id="title" />}
+      subTitle={<T id="subTitle" />}
+      description={<T id="description" values={{ br: () => <br /> }} />}
+      cta={
+        <LinkButton href={routes.organizationSettings.plans()}>
+          <T id="upgrade" />
+          <IconArrowRight className="size-icon" />
+        </LinkButton>
+      }
+      documentationLink={
+        <DocumentationLink path="/docs/run-and-scale/domains">
+          <T id="docs" />
+        </DocumentationLink>
+      }
+    />
   );
-};
+}
