6eda4b42189f97aafc1fa8f5e9cb709f9465d58d
nils <nilscox.dev@gmail.com>
2024-09-04 10:44:04 +0200

    subscribe to feature flags changes

--- src/hooks/feature-flag.ts
+++ src/hooks/feature-flag.ts
@@ -5,17 +5,10 @@ export function useFeatureFlag(feature: string) {
   const [enabled, setEnabled] = useState<boolean>();
 
   useEffect(() => {
-    // eslint-disable-next-line no-console
-    loadFeatureFlag(feature).then(setEnabled, console.error);
+    posthog.onFeatureFlags(() => {
+      setEnabled(posthog.isFeatureEnabled(feature) === true);
+    });
   }, [feature]);
 
   return enabled;
-}
-
-async function loadFeatureFlag(feature: string) {
-  await new Promise((resolve) => {
-    posthog.onFeatureFlags(resolve);
-  });
-
-  return posthog.isFeatureEnabled(feature) === true;
 }
