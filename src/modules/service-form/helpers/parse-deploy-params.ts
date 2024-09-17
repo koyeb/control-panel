a48f44d6391dd527d2e281eff9bde03315f8db77
nils <nilscox.dev@gmail.com>
2024-09-17 12:32:08 +0200

    add support for autoscaling and health checks when creating the deploy params

--- src/modules/service-form/helpers/parse-deploy-params.ts
+++ src/modules/service-form/helpers/parse-deploy-params.ts
@@ -553,7 +553,7 @@ export function getDeployParams(form: ServiceForm): URLSearchParams {
     name: form.serviceName,
   });
 
-  const set = (param: string, value: string | string[] | boolean | null) => {
+  const set = (param: string, value: string | string[] | number | boolean | null) => {
     if (value === null) {
       return;
     }
@@ -566,8 +566,8 @@ export function getDeployParams(form: ServiceForm): URLSearchParams {
       params.set(param, 'true');
     }
 
-    if (typeof value === 'string') {
-      params.set(param, value);
+    if (typeof value === 'string' || typeof value === 'number') {
+      params.set(param, String(value));
     }
   };
 
@@ -619,12 +619,45 @@ export function getDeployParams(form: ServiceForm): URLSearchParams {
   if (form.instance.identifier !== 'free') set('instance_type', form.instance.identifier);
   if (form.regions.length !== 1 || form.regions[0] !== 'fra') set('regions', form.regions);
 
+  if (form.scaling.type === 'fixed') {
+    if (form.scaling.fixed !== 1) {
+      set('instances_min', String(form.scaling.fixed));
+    }
+  } else {
+    set('instances_min', String(form.scaling.autoscaling.min));
+    set('instances_max', String(form.scaling.autoscaling.max));
+
+    for (const [target, { enabled, value }] of Object.entries(form.scaling.autoscaling.targets)) {
+      const targetMap: Record<keyof AutoScaling['targets'], string> = {
+        cpu: 'average_cpu',
+        memory: 'average_mem',
+        requests: 'requests_per_second',
+        concurrentRequests: 'concurrent_requests',
+        responseTime: 'requests_response_time',
+      };
+
+      if (enabled) {
+        set(`autoscaling_${targetMap[target as keyof AutoScaling['targets']]}`, String(value));
+      }
+    }
+  }
+
   for (const { name, value } of form.environmentVariables) {
     set(`env[${name}]`, value);
   }
 
-  for (const { portNumber, path } of form.ports) {
-    params.append('ports', [portNumber, 'http', path].join(';'));
+  for (const { portNumber, path, public: isPublic, protocol, healthCheck: hc } of form.ports) {
+    params.append('ports', [portNumber, protocol, isPublic ? path : undefined].filter(Boolean).join(';'));
+
+    const defaultHc = defaultHealthCheck();
+
+    if (hc.protocol !== defaultHc.protocol) set(`hc_protocol[${portNumber}]`, hc.protocol);
+    if (hc.gracePeriod !== defaultHc.gracePeriod) set(`hc_grace_period[${portNumber}]`, hc.gracePeriod);
+    if (hc.interval !== defaultHc.interval) set(`hc_interval[${portNumber}]`, hc.interval);
+    if (hc.restartLimit !== defaultHc.restartLimit) set(`hc_restart_limit[${portNumber}]`, hc.restartLimit);
+    if (hc.timeout !== defaultHc.timeout) set(`hc_timeout[${portNumber}]`, hc.timeout);
+    if (hc.path !== defaultHc.path) set(`hc_path[${portNumber}]`, hc.path);
+    if (hc.method !== defaultHc.method) set(`hc_method[${portNumber}]`, hc.method);
   }
 
   return params;
