202898f2fd52c5f5b458aa8e29bda37b84a693ea
nils <nilscox.dev@gmail.com>
2024-10-07 14:22:46 +0200

    remove custom theme provider for storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,14 +1,10 @@
-import { addons } from '@storybook/preview-api';
 import type { Preview } from '@storybook/react';
 import { Elements as StripeElements } from '@stripe/react-stripe-js';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useEffect } from 'react';
-import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
 
 import { AnalyticsProvider, NoopAnalytics } from '../src/application/analytics';
 import { ApiMock } from '../src/api/mock/mock-api';
 import { IntlProvider } from '../src/intl/translation-provider';
-import { ThemeMode, useThemeMode } from '../src/hooks/theme';
 import { TokenProvider } from '../src/application/token';
 
 import '@fontsource-variable/inter';
@@ -68,11 +64,6 @@ export default {
         <Story />
       </IntlProvider>
     ),
-    (Story) => (
-      <SetThemeMode>
-        <Story />
-      </SetThemeMode>
-    ),
     (Story, { args, parameters }) => {
       new ApiMock();
       parameters.mockApi?.(args);
@@ -93,23 +84,3 @@ const queryClient = new QueryClient({
     },
   },
 });
-
-const channel = addons.getChannel();
-
-function SetThemeMode({ children }: { children: React.ReactNode }) {
-  const [, setTheme] = useThemeMode();
-
-  useEffect(() => {
-    function handler(dark: boolean) {
-      setTheme(dark ? ThemeMode.dark : ThemeMode.light);
-    }
-
-    channel.on(DARK_MODE_EVENT_NAME, handler);
-
-    return () => {
-      channel.off(DARK_MODE_EVENT_NAME, handler);
-    };
-  }, []);
-
-  return children;
-}
