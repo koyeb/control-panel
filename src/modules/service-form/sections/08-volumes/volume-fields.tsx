57f4738e8000041d5b0516b408e9e212074cb6c8
nils <nilscox.dev@gmail.com>
2024-11-29 00:22:39 +0100

    refactor volume fields component

--- src/modules/service-form/sections/08-volumes/volume-fields.tsx
+++ src/modules/service-form/sections/08-volumes/volume-fields.tsx
@@ -22,44 +22,13 @@ export function VolumeFields({ index, onRemove }: { index: number; onRemove: () 
 
   const form = useFormContext<ServiceForm>();
 
-  const [regionIdentifier] = useWatchServiceForm('regions');
-  const region = useRegion(regionIdentifier);
-
-  const serviceId = useWatchServiceForm('meta.serviceId');
-  const volumes = useVolumes(regionIdentifier)?.filter(
-    (volume) => volume.serviceId === undefined || volume.serviceId === serviceId,
-  );
-
-  const formVolumes = useWatchServiceForm('volumes');
-
-  const volumesAndVolumesToCreate = useMemo(() => {
-    const volumesToCreate = formVolumes.filter(
-      ({ name }) => name !== '' && !volumes?.some(hasProperty('name', name)),
-    );
-
-    return [...(volumes ?? []), ...volumesToCreate];
-  }, [volumes, formVolumes]);
-
   const isMobile = !useBreakpoint('md');
   const showLabel = isMobile || index === 0;
-
-  const { setValue } = useFormContext<ServiceForm>();
 
   return (
     // eslint-disable-next-line tailwindcss/no-arbitrary-value
     <div className="grid grid-cols-1 gap-4 rounded border px-6 py-5 md:grid-cols-[1fr_1fr_1fr_auto] md:border-none md:p-0">
-      <ControlledSelect<ServiceForm, `volumes.${number}.name`, ServiceVolume | Volume>
-        name={`volumes.${index}.name`}
-        label={showLabel && <T id="volumeNameLabel" />}
-        placeholder={t('volumeNamePlaceholder')}
-        items={volumesAndVolumesToCreate}
-        getKey={getName}
-        itemToString={getName}
-        itemToValue={getName}
-        renderItem={getName}
-        renderNoItems={() => <T id="noVolumes" values={{ region: region?.displayName }} />}
-        onChangeEffect={(volume) => 'id' in volume && setValue(`volumes.${index}.volumeId`, volume.id)}
-      />
+      <VolumeField index={index} label={showLabel && <T id="volumeNameLabel" />} />
 
       <ControlledInput<ServiceForm, `volumes.${number}.mountPath`>
         name={`volumes.${index}.mountPath`}
@@ -82,4 +51,47 @@ export function VolumeFields({ index, onRemove }: { index: number; onRemove: () 
       </div>
     </div>
   );
+}
+
+function VolumeField({ index, label }: { index: number; label?: React.ReactNode }) {
+  const t = T.useTranslate();
+  const { setValue } = useFormContext<ServiceForm>();
+
+  const [regionIdentifier] = useWatchServiceForm('regions');
+  const region = useRegion(regionIdentifier);
+
+  const items = useVolumeItems();
+
+  return (
+    <ControlledSelect<ServiceForm, `volumes.${number}.name`, ServiceVolume | Volume>
+      name={`volumes.${index}.name`}
+      label={label}
+      placeholder={t('volumeNamePlaceholder')}
+      items={items}
+      getKey={getName}
+      itemToString={getName}
+      itemToValue={getName}
+      renderItem={getName}
+      renderNoItems={() => <T id="noVolumes" values={{ region: region?.displayName }} />}
+      onChangeEffect={(volume) => 'id' in volume && setValue(`volumes.${index}.volumeId`, volume.id)}
+    />
+  );
+}
+
+function useVolumeItems() {
+  const serviceId = useWatchServiceForm('meta.serviceId');
+  const [region] = useWatchServiceForm('regions');
+  const formVolumes = useWatchServiceForm('volumes');
+
+  const volumes = useVolumes(region)?.filter(
+    (volume) => volume.serviceId === undefined || volume.serviceId === serviceId,
+  );
+
+  return useMemo(() => {
+    const volumesToCreate = formVolumes.filter(
+      ({ name }) => name !== '' && !volumes?.some(hasProperty('name', name)),
+    );
+
+    return [...(volumes ?? []), ...volumesToCreate];
+  }, [volumes, formVolumes]);
 }
