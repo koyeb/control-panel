0f3a8a92cb491b3ca29d1bba6cc55db43aa5ef77
nils <nilscox.dev@gmail.com>
2024-12-02 16:05:57 +0100

    allow to override feature flags from the local storage

--- src/hooks/feature-flag.ts
+++ src/hooks/feature-flag.ts
@@ -1,14 +1,35 @@
 import posthog from 'posthog-js';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { z } from 'zod';
 
 export function useFeatureFlag(feature: string) {
   const [enabled, setEnabled] = useState<boolean>();
+  const localStorageFlags = useLocalStorageFlags();
 
   useEffect(() => {
-    posthog.onFeatureFlags(() => {
-      setEnabled(posthog.isFeatureEnabled(feature) === true);
-    });
-  }, [feature]);
+    if (feature in localStorageFlags) {
+      setEnabled(localStorageFlags[feature]);
+    } else {
+      posthog.onFeatureFlags(() => {
+        setEnabled(posthog.isFeatureEnabled(feature) === true);
+      });
+    }
+  }, [localStorageFlags, feature]);
 
   return enabled;
+}
+
+function useLocalStorageFlags() {
+  return useMemo(() => {
+    try {
+      const schema = z.record(z.string(), z.boolean());
+      const localStorageFlags: unknown = JSON.parse(localStorage.getItem('feature-flags') ?? '');
+
+      return schema.parse(localStorageFlags);
+    } catch {
+      //
+    }
+
+    return {};
+  }, []);
 }
