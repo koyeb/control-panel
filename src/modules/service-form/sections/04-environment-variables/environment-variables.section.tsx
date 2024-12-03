2828c2ce501fc33e15f5e03ae4499dd46b355dff
nils <nilscox.dev@gmail.com>
2024-12-03 18:30:59 +0100

    add tabs to the environment variables section for the file mounts

--- src/modules/service-form/sections/04-environment-variables/environment-variables.section.tsx
+++ src/modules/service-form/sections/04-environment-variables/environment-variables.section.tsx
@@ -1,16 +1,25 @@
+import { useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
+import { TabButton, TabButtons } from '@koyeb/design-system';
+import { useFeatureFlag } from 'src/hooks/feature-flag';
 import { Translate } from 'src/intl/translate';
 
 import { ServiceFormSection } from '../../components/service-form-section';
 import { ServiceForm } from '../../service-form.types';
 
 import { EnvironmentVariables } from './environment-variables';
+import { Files } from './files';
 
 const T = Translate.prefix('serviceForm.environmentVariables');
 
+const tabs = ['environmentVariables', 'files'] as const;
+
 export function EnvironmentVariablesSection() {
   const variables = useFormContext<ServiceForm>().watch('environmentVariables');
+  const [currentTab, setCurrentTab] = useState<(typeof tabs)[number]>(tabs[0]);
+
+  const hasMountFiles = useFeatureFlag('mount-files');
 
   return (
     <ServiceFormSection
@@ -18,8 +27,20 @@ export function EnvironmentVariablesSection() {
       title={<T id="title" values={{ count: variables.filter((field) => field.name !== '').length }} />}
       description={<T id="description" />}
       expandedTitle={<T id="expandedTitle" />}
+      className="col gaps"
     >
-      <EnvironmentVariables />
+      {hasMountFiles && (
+        <TabButtons>
+          {tabs.map((tab) => (
+            <TabButton key={tab} selected={tab === currentTab} onClick={() => setCurrentTab(tab)}>
+              <T id={`tabs.${tab}`} />
+            </TabButton>
+          ))}
+        </TabButtons>
+      )}
+
+      {currentTab === 'environmentVariables' && <EnvironmentVariables />}
+      {currentTab === 'files' && <Files />}
     </ServiceFormSection>
   );
 }
