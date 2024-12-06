29388c11d243762edb6bcbc118cc5be9c247af7f
nils <nilscox.dev@gmail.com>
2024-12-06 13:48:48 +0100

    fix github oauth

--- src/pages/account/account.pages.tsx
+++ src/pages/account/account.pages.tsx
@@ -1,8 +1,6 @@
 // eslint-disable-next-line no-restricted-imports
 import { Route, Switch } from 'wouter';
 
-import { useOrganizationQuery, useUserQuery } from 'src/api/hooks/session';
-import { Loading } from 'src/components/loading';
 import { SecondaryLayout } from 'src/layouts/secondary/secondary-layout';
 
 import { ChangePasswordPage } from './change-password';
@@ -11,17 +9,6 @@ import { InvitationPage } from './invitation.page';
 import { ValidateAccountPage } from './validate-account.page';
 
 export function AccountPages() {
-  const userQuery = useUserQuery();
-  const organizationQuery = useOrganizationQuery();
-
-  if (!userQuery.isSuccess || organizationQuery.isPending) {
-    return (
-      <SecondaryLayout>
-        <Loading />
-      </SecondaryLayout>
-    );
-  }
-
   return (
     <SecondaryLayout>
       <Switch>
