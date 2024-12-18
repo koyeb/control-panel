b706bfe2a447229727de4d44d3a5d56fce89aae7
nils <nilscox.dev@gmail.com>
2024-12-18 17:18:58 +0100

    show a confirmation dialog before bulk deleting secrets

---
+++ src/pages/secrets/components/bulk-delete-secret-dialog.tsx
@@ -0,0 +1,53 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { api } from 'src/api/api';
+import { Secret } from 'src/api/model';
+import { useInvalidateApiQuery } from 'src/api/use-api';
+import { notify } from 'src/application/notify';
+import { useToken } from 'src/application/token';
+import { ConfirmationDialog } from 'src/components/confirmation-dialog';
+import { Translate } from 'src/intl/translate';
+
+const T = Translate.prefix('pages.secrets.bulkDelete');
+
+type BulkDeleteSecretsDialogProps = {
+  open: boolean;
+  onClose: () => void;
+  secrets: Secret[];
+  onDeleted: () => void;
+};
+
+export function BulkDeleteSecretsDialog({ open, onClose, secrets, onDeleted }: BulkDeleteSecretsDialogProps) {
+  const t = T.useTranslate();
+
+  const { token } = useToken();
+  const invalidate = useInvalidateApiQuery();
+
+  const mutation = useMutation({
+    async mutationFn(secrets: Secret[]) {
+      return Promise.allSettled(
+        secrets.map((secret) => api.deleteSecret({ token, path: { id: secret.id } })),
+      );
+    },
+    async onSuccess(result) {
+      await invalidate('listSecrets');
+
+      const fulfilled = result.filter((result) => result.status === 'fulfilled');
+      notify.success(t('successNotification', { count: fulfilled.length }));
+
+      onDeleted();
+    },
+  });
+
+  return (
+    <ConfirmationDialog
+      open={open}
+      onClose={onClose}
+      title={<T id="title" />}
+      description={<T id="description" values={{ count: secrets.length }} />}
+      confirmationText={t('confirmationText')}
+      submitText={<T id="cta" />}
+      onConfirm={() => mutation.mutateAsync(secrets)}
+    />
+  );
+}
--- src/pages/secrets/secrets.page.tsx
+++ src/pages/secrets/secrets.page.tsx
@@ -1,14 +1,9 @@
-import { useMutation } from '@tanstack/react-query';
 import clsx from 'clsx';
 import { useState } from 'react';
 
 import { Button } from '@koyeb/design-system';
-import { api } from 'src/api/api';
 import { useSecretsQuery } from 'src/api/hooks/secret';
 import { Secret } from 'src/api/model';
-import { useInvalidateApiQuery } from 'src/api/use-api';
-import { notify } from 'src/application/notify';
-import { useToken } from 'src/application/token';
 import { DocumentTitle } from 'src/components/document-title';
 import { IconListPlus, IconPlus } from 'src/components/icons';
 import { Title } from 'src/components/title';
@@ -18,37 +13,22 @@ import { Translate } from 'src/intl/translate';
 import { CreateSecretDialog } from 'src/modules/secrets/simple/create-secret-dialog';
 
 import { BulkCreateSecretsDialog } from './components/bulk-create-secrets-dialog';
+import { BulkDeleteSecretsDialog } from './components/bulk-delete-secret-dialog';
 import { SecretsList } from './components/secrets-list';
 
 const T = Translate.prefix('pages.secrets');
 
 export function SecretsPage() {
-  const t = T.useTranslate();
-
   const historyState = useHistoryState<{ create: boolean }>();
-  const [createDialogOpen, setCreateDialogOpen] = useState(Boolean(historyState.create));
-  const [bulkCreateDialogOpen, setBulkCreateDialogOpen] = useState(false);
+  const [openDialog, setOpenDialog] = useState<'create' | 'bulkCreate' | 'bulkDelete' | undefined>(
+    historyState.create ? 'create' : undefined,
+  );
+
+  const closeDialog = () => setOpenDialog(undefined);
+
   const { data: secrets } = useSecretsQuery('simple');
 
-  const { token } = useToken();
-  const invalidate = useInvalidateApiQuery();
   const [selected, { toggle, clear }] = useSet<Secret>();
-
-  const bulkDelete = useMutation({
-    async mutationFn(secrets: Secret[]) {
-      return Promise.allSettled(
-        secrets.map((secret) => api.deleteSecret({ token, path: { id: secret.id } })),
-      );
-    },
-    async onSuccess(result) {
-      await invalidate('listSecrets');
-
-      clear();
-
-      const fulfilled = result.filter((result) => result.status === 'fulfilled');
-      notify.success(t('bulkDeleted', { count: fulfilled.length }));
-    },
-  });
 
   return (
     <div className="col gap-8">
@@ -59,23 +39,19 @@ export function SecretsPage() {
         end={
           <div className="row items-center gap-2">
             {selected.size > 0 && (
-              <Button
-                variant="outline"
-                loading={bulkDelete.isPending}
-                onClick={() => bulkDelete.mutate(Array.from(selected.values()))}
-              >
+              <Button variant="outline" onClick={() => setOpenDialog('bulkDelete')}>
                 <T id="deleteSecrets" values={{ count: selected.size }} />
               </Button>
             )}
 
-            <Button variant="outline" onClick={() => setBulkCreateDialogOpen(true)}>
+            <Button variant="outline" onClick={() => setOpenDialog('bulkCreate')}>
               <IconListPlus className="size-4" />
               <T id="importSecrets" />
             </Button>
 
             <Button
               className={clsx(secrets && secrets.length === 0 && 'hidden')}
-              onClick={() => setCreateDialogOpen(true)}
+              onClick={() => setOpenDialog('create')}
             >
               <IconPlus className="size-4" />
               <T id="createSecret" />
@@ -84,15 +60,20 @@ export function SecretsPage() {
         }
       />
 
-      <SecretsList onCreate={() => setCreateDialogOpen(true)} selected={selected} toggleSelected={toggle} />
+      <SecretsList onCreate={() => setOpenDialog('create')} selected={selected} toggleSelected={toggle} />
 
-      <CreateSecretDialog
-        open={createDialogOpen}
-        onClose={() => setCreateDialogOpen(false)}
-        onCreated={() => setCreateDialogOpen(false)}
+      <BulkDeleteSecretsDialog
+        open={openDialog === 'bulkDelete'}
+        onClose={closeDialog}
+        secrets={Array.from(selected.values())}
+        onDeleted={() => {
+          clear();
+          closeDialog();
+        }}
       />
 
-      <BulkCreateSecretsDialog open={bulkCreateDialogOpen} onClose={() => setBulkCreateDialogOpen(false)} />
+      <BulkCreateSecretsDialog open={openDialog === 'bulkCreate'} onClose={closeDialog} />
+      <CreateSecretDialog open={openDialog === 'create'} onClose={closeDialog} onCreated={closeDialog} />
     </div>
   );
 }
