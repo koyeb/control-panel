4785a7722081312740e363c7c4c2cb6beae5a8ce
nils <nilscox.dev@gmail.com>
2024-12-18 17:40:25 +0100

    add an indeterminate prop to the checkbox component

--- design-system/src/checkbox/checkbox.tsx
+++ design-system/src/checkbox/checkbox.tsx
@@ -10,7 +10,7 @@ type CheckboxOwnProps = {
 };
 
 type CheckboxProps = CheckboxOwnProps &
-  Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type' | keyof CheckboxOwnProps>;
+  Omit<React.ComponentProps<typeof CheckboxInput>, 'type' | keyof CheckboxOwnProps>;
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(function Checkbox(
   { label, helpTooltip, className, ...props },
@@ -37,23 +37,24 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(function Che
   );
 });
 
-export const CheckboxInput = forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
-  function CheckboxInput(props, ref) {
-    return (
-      <>
-        <input ref={ref} type="checkbox" className="peer sr-only" {...props} />
+export const CheckboxInput = forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<'input'> & { indeterminate?: boolean }
+>(function CheckboxInput({ indeterminate, ...props }, ref) {
+  return (
+    <>
+      <input ref={ref} type="checkbox" className="peer sr-only" {...props} />
 
-        <span className="leading-none peer-checked:hidden peer-disabled:[&>span]:bg-muted">
-          <span className="inline-block size-4 rounded border" />
-        </span>
+      <span className="leading-none peer-checked:hidden peer-disabled:[&>span]:bg-muted">
+        <span className="inline-block size-4 rounded border" />
+      </span>
 
-        <span className="hidden leading-none peer-checked:block">
-          <Checked className="size-4" />
-        </span>
-      </>
-    );
-  },
-);
+      <span className="hidden leading-none peer-checked:block">
+        {indeterminate ? <Indeterminate className="size-4" /> : <Checked className="size-4" />}
+      </span>
+    </>
+  );
+});
 
 function Checked(props: React.SVGProps<SVGSVGElement>) {
   return (
@@ -69,6 +70,28 @@ function Checked(props: React.SVGProps<SVGSVGElement>) {
         strokeWidth="1.6666"
         strokeLinecap="round"
         strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+function Indeterminate(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 16 16" fill="none" {...props}>
+      <path
+        d="M0 4C0 1.79086 1.79086 0 4 0H12C14.2091 0 16 1.79086 16 4V12C16 14.2091 14.2091 16 12 16H4C1.79086 16 0 14.2091 0 12V4Z"
+        fill="#059669"
+        fillOpacity="0.1"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M5.33337 8.00013C5.33337 7.70558 5.57216 7.4668 5.86671 7.4668H10.1334C10.4279 7.4668 10.6667 7.70558 10.6667 8.00013C10.6667 8.29468 10.4279 8.53346 10.1334 8.53346H5.86671C5.57216 8.53346 5.33337 8.29468 5.33337 8.00013Z"
+        fill="#059669"
+      />
+      <path
+        d="M0.5 4C0.5 2.067 2.067 0.5 4 0.5H12C13.933 0.5 15.5 2.067 15.5 4V12C15.5 13.933 13.933 15.5 12 15.5H4C2.067 15.5 0.5 13.933 0.5 12V4Z"
+        stroke="#059669"
       />
     </svg>
   );
