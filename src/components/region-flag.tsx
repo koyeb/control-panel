65fa681c99bb84b01df07b97b5aebaac9d392f5e
nils <nilscox.dev@gmail.com>
2025-01-29 11:13:51 +0100

    add continental regions flags

--- src/components/region-flag.tsx
+++ src/components/region-flag.tsx
@@ -1,6 +1,12 @@
+import af from 'src/icons/flags/AF.png';
 import de from 'src/icons/flags/DE.png';
+import eu from 'src/icons/flags/EU.png';
 import fr from 'src/icons/flags/FR.png';
 import jp from 'src/icons/flags/JP.png';
+import na from 'src/icons/flags/NA.png';
+import nea from 'src/icons/flags/NEA.png';
+import sa from 'src/icons/flags/SA.png';
+import sea from 'src/icons/flags/SEA.png';
 import sg from 'src/icons/flags/SG.png';
 import us from 'src/icons/flags/US.png';
 
@@ -19,6 +25,12 @@ export function RegionFlag({ identifier, ...props }: RegionFlagProps) {
 }
 
 const map: Record<string, string> = {
+  na,
+  sa,
+  nea,
+  sea,
+  af,
+  eu,
   fra: de,
   par: fr,
   sfo: us,
