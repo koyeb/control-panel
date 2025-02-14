4eb11b06a18cf541ec96a0b1c4c1c0ae797d16fd
nils <nilscox.dev@gmail.com>
2025-02-14 17:51:34 +0100

    add a small variant to the tab button component

--- design-system/src/tab-buttons/tab-buttons.tsx
+++ design-system/src/tab-buttons/tab-buttons.tsx
@@ -1,23 +1,32 @@
 import { cva } from 'class-variance-authority';
 
 type TabButtonsProps = {
+  size?: 1 | 2;
   className?: string;
   children?: React.ReactNode;
 };
 
-export function TabButtons({ className, children }: TabButtonsProps) {
+export function TabButtons({ size = 2, className, children }: TabButtonsProps) {
   return (
     <div className="max-w-full overflow-x-auto">
-      <div role="tablist" className={tabButtons({ className })}>
+      <div role="tablist" className={TabButtons.class({ size, className })}>
         {children}
       </div>
     </div>
   );
 }
 
-const tabButtons = cva('row w-fit gap-2 rounded-md bg-muted p-1');
+TabButtons.class = cva('row w-fit gap-2 rounded-md bg-muted p-1', {
+  variants: {
+    size: {
+      1: 'h-8',
+      2: 'h-10',
+    },
+  },
+});
 
 type TabButtonProps = {
+  size?: 1 | 2;
   selected: boolean;
   disabled?: boolean;
   panelId?: string;
@@ -26,13 +35,21 @@ type TabButtonProps = {
   children?: React.ReactNode;
 };
 
-export function TabButton({ selected, disabled, panelId, onClick, className, children }: TabButtonProps) {
+export function TabButton({
+  size,
+  selected,
+  disabled,
+  panelId,
+  onClick,
+  className,
+  children,
+}: TabButtonProps) {
   return (
     <button
       type="button"
       role="tab"
       disabled={disabled}
-      className={tabButton({ className, selected })}
+      className={TabButton.class({ size, selected, className })}
       aria-selected={selected}
       aria-controls={panelId}
       onClick={onClick}
@@ -42,13 +59,18 @@ export function TabButton({ selected, disabled, panelId, onClick, className, chi
   );
 }
 
-const tabButton = cva(
+TabButton.class = cva(
   [
-    'col focusable flex-1 items-center whitespace-nowrap rounded px-3 py-2 font-medium transition-all',
+    'col h-full flex-1 items-center justify-center',
+    'focusable whitespace-nowrap rounded px-3 transition-all',
     'disabled:pointer-events-none disabled:opacity-50',
   ],
   {
     variants: {
+      size: {
+        1: 'text-xs',
+        2: 'font-medium',
+      },
       selected: {
         false: 'text-dim hover:bg-neutral/50 hover:text-default',
         true: 'bg-neutral',
