9e4d1e00f53fc604a82d279294e0e19e8cf2ef2e
nils <nilscox.dev@gmail.com>
2025-02-20 12:43:22 +0100

    implement the new authentication layout

---
+++ src/layouts/authentication/authentication.layout.tsx
@@ -0,0 +1,33 @@
+import { lazy } from 'react';
+
+import LogoKoyeb from 'src/components/logo-koyeb.svg?react';
+
+import { Slides } from './slides';
+
+const Customers = lazy(() => import('./customers'));
+
+export function AuthenticationLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="row h-screen">
+      <div className="col flex-1 justify-between p-16">
+        <div>
+          <LogoKoyeb className="h-8" />
+        </div>
+
+        {children}
+
+        <div className="col items-center gap-3 text-[#6B6965]">
+          <div className="text-xs font-medium">Trusted by the most ambitious teams</div>
+          <div className="row flex-wrap justify-evenly gap-x-4 gap-y-3 px-32">
+            <Customers />
+          </div>
+        </div>
+      </div>
+
+      {/* eslint-disable-next-line tailwindcss/no-arbitrary-value */}
+      <div className="m-4 w-[44rem]">
+        <Slides />
+      </div>
+    </div>
+  );
+}
