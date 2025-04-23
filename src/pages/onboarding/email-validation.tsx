61439f8e6149b934421dec6cbf93aed8d787041b
nils <nilscox.dev@gmail.com>
2025-04-23 13:39:29 +0200

    change the layout of the email validation page

--- src/pages/onboarding/email-validation.tsx
+++ src/pages/onboarding/email-validation.tsx
@@ -1,10 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { Button, Stepper } from '@koyeb/design-system';
 import { useUser } from 'src/api/hooks/session';
 import { useApiMutationFn } from 'src/api/use-api';
 import { notify } from 'src/application/notify';
+import { routes } from 'src/application/routes';
+import { useToken } from 'src/application/token';
+import { IconSend } from 'src/components/icons';
+import { useNavigate } from 'src/hooks/router';
 import { createTranslate } from 'src/intl/translate';
+
+import { AuthButton } from '../authentication/components/auth-button';
+
+import Globe from './images/globe.svg?react';
 
 const T = createTranslate('pages.onboarding.emailValidation');
 
@@ -12,37 +19,67 @@ export function EmailValidation() {
   const user = useUser();
   const t = T.useTranslate();
 
-  const mutation = useMutation({
+  const resendMutation = useMutation({
     ...useApiMutationFn('resendValidationEmail', {}),
     onSuccess() {
       notify.success(t('resendEmailSuccessNotification', { email: user.email }));
     },
   });
 
+  const { clearToken } = useToken();
+  const navigate = useNavigate();
+
+  const logoutMutation = useMutation({
+    ...useApiMutationFn('logout', {}),
+    onMutate: clearToken,
+    onSuccess: () => navigate(routes.home()),
+  });
+
   return (
-    <section className="col w-full max-w-xl items-start gap-6">
-      <Stepper totalSteps={3} activeStep={1} />
+    <section className="col w-full max-w-md flex-1 items-center justify-center gap-12">
+      <Globe className="absolute bottom-0" />
 
-      <h1 className="typo-heading">
-        <T id="title" />
-      </h1>
+      <div className="col gap-6 text-center">
+        <h1 className="text-3xl font-semibold">
+          <T id="title" />
+        </h1>
 
-      <div className="col max-w-sm gap-4 text-dim">
-        <p className="font-medium">
-          <T id="line1" values={{ email: user.email, green }} />
+        <p className="text-base text-dim">
+          <T
+            id="line1"
+            values={{
+              email: user.email,
+              strong: (children) => <strong className="text-default">{children}</strong>,
+            }}
+          />
         </p>
-        <p>
+
+        <p className="text-base text-dim">
           <T id="line2" />
         </p>
       </div>
 
-      <Button loading={mutation.isPending} onClick={() => mutation.mutate()}>
+      <AuthButton onClick={() => resendMutation.mutate()}>
+        <IconSend className="size-4" />
         <T id="resendValidationEmail" />
-      </Button>
+      </AuthButton>
+
+      <p className="text-dim">
+        <T
+          id="wrongEmail"
+          values={{
+            logout: (children) => (
+              <button
+                type="button"
+                onClick={() => logoutMutation.mutate()}
+                className="text-default underline"
+              >
+                {children}
+              </button>
+            ),
+          }}
+        />
+      </p>
     </section>
   );
 }
-
-const green = (children: React.ReactNode) => {
-  return <span className="text-green">{children}</span>;
-};
