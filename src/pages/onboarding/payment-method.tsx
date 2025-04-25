7dc49233a4d85a662d001d81158516e532f32ef1
nils <nilscox.dev@gmail.com>
2025-04-25 17:44:13 +0200

    add the billing alert to the payment form in the onboarding

--- src/pages/onboarding/payment-method.tsx
+++ src/pages/onboarding/payment-method.tsx
@@ -1,9 +1,18 @@
-import { InputEnd, InputStart } from '@koyeb/design-system';
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+
+import { InputEnd, InputStart, Spinner } from '@koyeb/design-system';
+import { useOrganization, useUser } from 'src/api/hooks/session';
+import { Address, OrganizationPlan } from 'src/api/model';
+import { useApiMutationFn, useInvalidateApiQuery } from 'src/api/use-api';
+import { notify } from 'src/application/notify';
 import { StripeProvider } from 'src/application/stripe';
+import { ControlledAddressField } from 'src/components/address-field/address-field';
 import { ControlledInput } from 'src/components/controlled';
 import { IconArrowLeftRight } from 'src/components/icons';
-import { PaymentFormFields, usePaymentForm } from 'src/components/payment-form';
-import { handleSubmit } from 'src/hooks/form';
+import { PaymentFormFields, PaymentMethodTimeout } from 'src/components/payment-form';
+import { FormValues, handleSubmit, useFormErrorHandler } from 'src/hooks/form';
+import { usePaymentMethodMutation } from 'src/hooks/stripe';
 import { createTranslate } from 'src/intl/translate';
 
 import { AuthButton } from '../authentication/components/auth-button';
@@ -21,10 +30,82 @@ export function PaymentMethod() {
 }
 
 function Form() {
-  const { form, mutation } = usePaymentForm('starter');
+  const t = T.useTranslate();
+
+  const user = useUser();
+  const organization = useOrganization();
+
+  const invalidate = useInvalidateApiQuery();
+
+  const form = useForm<{ address: Address; billingAlertAmount: number }>({
+    defaultValues: {
+      address: organization.billing.address ?? {
+        line1: '',
+        postalCode: '',
+        city: '',
+        country: '',
+      },
+      billingAlertAmount: NaN,
+    },
+  });
+
+  const billingInfoMutation = useMutation({
+    ...useApiMutationFn('updateOrganization', (address: Address) => ({
+      path: { id: organization.id },
+      query: {},
+      body: {
+        address1: address.line1,
+        address2: address.line2,
+        city: address.city,
+        postal_code: address.postalCode,
+        state: address.state,
+        country: address.country,
+        billing_name: organization.billing.name === undefined ? user.name : undefined,
+        billing_email: organization.billing.email === undefined ? user.email : undefined,
+      },
+    })),
+    onError: useFormErrorHandler(form, (error) => ({
+      'address.line1': error.address1,
+      'address.line2': error.address2,
+      'address.city': error.city,
+      'address.postalCode': error.postal_code,
+      'address.state': error.state,
+      'address.country': error.country,
+    })),
+  });
+
+  const changePlanMutation = useMutation({
+    ...useApiMutationFn('changePlan', (plan: OrganizationPlan) => ({
+      path: { id: organization.id },
+      body: { plan },
+    })),
+  });
+
+  const updateBudgetMutation = useMutation({
+    ...useApiMutationFn('updateBudget', (amount: number) => ({
+      path: { organization_id: organization.id },
+      body: { amount: String(100 * amount) },
+    })),
+  });
+
+  const paymentMethodMutation = usePaymentMethodMutation({
+    onTimeout: () => notify.error(<PaymentMethodTimeout />),
+  });
+
+  const onSubmit = async ({ address, billingAlertAmount }: FormValues<typeof form>) => {
+    await billingInfoMutation.mutateAsync(address);
+    await paymentMethodMutation.mutateAsync();
+    await changePlanMutation.mutateAsync('starter');
+
+    if (billingAlertAmount) {
+      await updateBudgetMutation.mutateAsync(billingAlertAmount);
+    }
+
+    await invalidate('getCurrentOrganization');
+  };
 
   return (
-    <form onSubmit={handleSubmit(form, mutation.mutateAsync)} className="col gap-8">
+    <form onSubmit={handleSubmit(form, onSubmit)} className="col gap-8">
       <section className="col gap-8">
         <header className="col gap-1">
           <h1 className="text-3xl font-semibold">
@@ -37,7 +118,16 @@ function Form() {
         </header>
 
         <div className="col gap-4">
-          <PaymentFormFields form={form} />
+          <PaymentFormFields />
+
+          <ControlledAddressField
+            control={form.control}
+            name="address"
+            required
+            size={3}
+            label={<T id="creditCard.addressLabel" />}
+            placeholder={t('creditCard.addressPlaceholder')}
+          />
 
           <div className="row items-center gap-3 rounded-md bg-gray/10 px-3 py-2">
             <IconArrowLeftRight className="size-4 text-dim" />
@@ -80,7 +170,8 @@ function Form() {
         </div>
       </section>
 
-      <AuthButton type="submit" loading={form.formState.isSubmitting} className="self-start">
+      <AuthButton type="submit" className="self-start">
+        {form.formState.isSubmitting && <Spinner className="size-4" />}
         <T id="submit" />
       </AuthButton>
     </form>
