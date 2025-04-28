64ef4a88fd2889916ece3852d7bf3da0536e65bb
nils <nilscox.dev@gmail.com>
2025-04-28 12:59:00 +0200

    show the join organization screen in the full screen layout

--- src/layouts/onboarding/full-screen-layout.tsx
+++ src/layouts/onboarding/full-screen-layout.tsx
@@ -1,6 +1,9 @@
 import LogoKoyeb from 'src/components/logo-koyeb.svg?react';
+import { useForceThemeMode, ThemeMode } from 'src/hooks/theme';
 
-export function EmailValidationLayout({ children }: { children: React.ReactNode }) {
+export function FullScreenLayout({ children }: { children: React.ReactNode }) {
+  useForceThemeMode(ThemeMode.light);
+
   return (
     <div className="col h-screen p-3">
       <div className="col dark relative flex-1 items-center rounded-2xl bg-neutral/95 p-16">
