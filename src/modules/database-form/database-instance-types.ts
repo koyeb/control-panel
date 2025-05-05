c63a6e96381274d11bf276c606c03808b1bb870a
nils <nilscox.dev@gmail.com>
2025-05-05 14:41:54 +0200

    update database instance types

--- src/modules/database-form/database-instance-types.ts
+++ src/modules/database-form/database-instance-types.ts
@@ -2,10 +2,14 @@ import { CatalogInstance } from 'src/api/model';
 
 // prettier-ignore
 export const databaseInstances = Object.freeze([
-  createDatabaseInstanceType('free',    'Free',     [0.25,   1,    1],      0,      0,        0),
-  createDatabaseInstanceType('small',   'Small',    [0.25,   1, null],  20.89, 0.0288, 0.000008),
-  createDatabaseInstanceType('medium',  'Medium',   [ 0.5,   2, null],  42.85, 0.0576, 0.000016),
-  createDatabaseInstanceType('large',   'Large',    [   1,   4, null],  96.42, 0.1296, 0.000036),
+  //                         id         display_name   cpu   ram   disk    price/mo  price/hr
+  createDatabaseInstanceType('free',    'Free',     [ 0.25,    1,     1],         0,        0),
+  createDatabaseInstanceType('small',   'Small',    [ 0.25,    1,  null],     29.76,     0.04),
+  createDatabaseInstanceType('medium',  'Medium',   [  0.5,    2,  null],     59.52,     0.08),
+  createDatabaseInstanceType('large',   'Large',    [    1,    4,  null],    119.04,     0.16),
+  createDatabaseInstanceType('xlarge',  'XLarge',   [    2,    8,  null],    238.08,     0.32),
+  createDatabaseInstanceType('2xlarge', '2XLarge',  [    4,   16,  null],    476.16,     0.64),
+  createDatabaseInstanceType('3xlarge', '3XLarge',  [    8,   32,  null],    952.32,    0.128),
 ])
 
 function createDatabaseInstanceType(
@@ -14,7 +18,6 @@ function createDatabaseInstanceType(
   [vcpuShares, memory, disk]: [cpu: number, ram: number, disk: number | null],
   priceMonthly: number,
   priceHourly: number,
-  pricePerSecond: number,
 ): CatalogInstance {
   return {
     id: catalogInstanceId,
@@ -27,7 +30,7 @@ function createDatabaseInstanceType(
     disk: disk === null ? '∞' : disk + ' GB',
     priceMonthly,
     priceHourly,
-    pricePerSecond,
+    pricePerSecond: priceHourly / (60 * 60),
     plans:
       catalogInstanceId !== 'free'
         ? ['starter', 'startup', 'pro', 'scale', 'business', 'enterprise', 'internal']
