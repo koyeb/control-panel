770cd02522b7a5ba29dc6cb370ef8a0b5f49af34
nils <nilscox.dev@gmail.com>
2025-06-11 13:14:11 +0200

    allow to access the secondary settings from the onboarding

--- src/layouts/onboarding/full-screen-layout.tsx
+++ src/layouts/onboarding/full-screen-layout.tsx
@@ -1,13 +1,33 @@
+import { isApiError } from 'src/api/api-errors';
+import { useUserQuery } from 'src/api/hooks/session';
 import LogoKoyeb from 'src/components/logo-koyeb.svg?react';
-import { useForceThemeMode, ThemeMode } from 'src/hooks/theme';
+import { useSearchParams } from 'src/hooks/router';
+import { ThemeMode, useForceThemeMode } from 'src/hooks/theme';
+
+import { SecondarySettings } from '../secondary/settings';
+import { UserMenu } from '../secondary/user-menu';
 
 export function FullScreenLayout({ children }: { children: React.ReactNode }) {
+  const params = useSearchParams();
+  const userQuery = useUserQuery();
+
+  const accountLocked = userQuery.isError && isApiError(userQuery.error) && userQuery.error.status === 403;
+  const isAuthenticated = userQuery.isSuccess || accountLocked;
+
   useForceThemeMode(ThemeMode.light);
+
+  if (params.has('settings')) {
+    return <SecondarySettings />;
+  }
 
   return (
     <div className="col h-screen p-3">
       <div className="col dark relative flex-1 items-center rounded-2xl bg-neutral/95 p-16">
-        <LogoKoyeb className="h-8 self-start" />
+        <div className="row justify-between self-stretch">
+          <LogoKoyeb className="h-8 self-start" />
+          {isAuthenticated && <UserMenu />}
+        </div>
+
         {children}
       </div>
     </div>
