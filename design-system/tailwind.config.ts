c9fb4566c405da095015c478dcc3150185ff18da
nils <nilscox.dev@gmail.com>
2025-06-22 02:56:25 +0200

    move the design system code

--- design-system/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'tailwindcss';
-
-import preset from './tailwind-preset';
-
-export default {
-  content: ['src/**/*.tsx'],
-  darkMode: 'selector',
-  presets: [preset],
-} satisfies Config;
