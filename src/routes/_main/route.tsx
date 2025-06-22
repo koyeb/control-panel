1b7fd6dbc3b0f24d06c4bd4180480be19d78329b
nils <nilscox.dev@gmail.com>
2025-06-23 01:20:57 +0200

    handle account locked

--- src/routes/_main/route.tsx
+++ src/routes/_main/route.tsx
@@ -1,7 +1,7 @@
 import { QueryClient } from '@tanstack/react-query';
 import { createFileRoute, Outlet, ParsedLocation } from '@tanstack/react-router';
 
-import { ApiError } from 'src/api/api-errors';
+import { ApiError, isAccountLockedError } from 'src/api/api-errors';
 import { mapCatalogDatacenter } from 'src/api/mappers/catalog';
 import { mapOrganization } from 'src/api/mappers/session';
 import { useApiQueryFn } from 'src/api/use-api';
@@ -9,13 +9,22 @@ import { redirectToSignIn, setToken } from 'src/application/authentication';
 import { useToken } from 'src/application/token';
 import { getUrlLatency } from 'src/application/url-latency';
 import { MainLayout } from 'src/layouts/main/main-layout';
+import { AccountLocked } from 'src/modules/account/account-locked';
 
 export const Route = createFileRoute('/_main')({
-  component: () => (
-    <MainLayout>
-      <Outlet />
-    </MainLayout>
-  ),
+  component: function Component() {
+    const { locked } = Route.useLoaderData();
+
+    if (locked) {
+      return <AccountLocked />;
+    }
+
+    return (
+      <MainLayout>
+        <Outlet />
+      </MainLayout>
+    );
+  },
 
   beforeLoad: ({ location }) => {
     if (!useToken().token) {
@@ -26,9 +35,21 @@ export const Route = createFileRoute('/_main')({
   loader: async ({ context, location }) => {
     const { queryClient } = context;
 
-    await fetchCurrentSession(queryClient, location);
-    await fetchCatalog(queryClient);
-    await preloadDatacenterLatencies(queryClient);
+    try {
+      await fetchCurrentSession(queryClient, location);
+      await fetchCatalog(queryClient);
+      await preloadDatacenterLatencies(queryClient);
+    } catch (error) {
+      if (isAccountLockedError(error)) {
+        return { locked: true };
+      }
+
+      throw error;
+    }
+
+    return {
+      locked: false,
+    };
   },
 });
 
@@ -38,21 +59,34 @@ async function fetchCurrentSession(queryClient: QueryClient, location: ParsedLoc
       queryClient.ensureQueryData(useApiQueryFn('getCurrentUser')),
       queryClient
         .ensureQueryData(useApiQueryFn('getCurrentOrganization'))
-        .then(({ organization }) => mapOrganization(organization!)),
+        .then(({ organization }) => mapOrganization(organization!))
+        .catch((error) => {
+          if (error instanceof ApiError && error.status === 404) {
+            return null;
+          }
+
+          throw error;
+        }),
     ]);
 
-    const queries = new Array<ReturnType<typeof useApiQueryFn>>();
-
-    if (organization.latestSubscriptionId) {
-      queries.push(useApiQueryFn('getSubscription', { path: { id: organization.latestSubscriptionId } }));
-    }
-
     if (organization) {
-      queries.push(useApiQueryFn('organizationSummary', { path: { organization_id: organization.id } }));
-      queries.push(useApiQueryFn('organizationQuotas', { path: { organization_id: organization.id } }));
-    }
+      if (organization.statusMessage === 'VERIFICATION_FAILED') {
+        throw { status: 403, message: 'Account is locked' };
+      }
 
-    await Promise.all(queries.map((query) => queryClient.ensureQueryData(query)));
+      const queries = new Array<ReturnType<typeof useApiQueryFn>>();
+
+      if (organization.latestSubscriptionId) {
+        queries.push(useApiQueryFn('getSubscription', { path: { id: organization.latestSubscriptionId } }));
+      }
+
+      if (organization) {
+        queries.push(useApiQueryFn('organizationSummary', { path: { organization_id: organization.id } }));
+        queries.push(useApiQueryFn('organizationQuotas', { path: { organization_id: organization.id } }));
+      }
+
+      await Promise.all(queries.map((query) => queryClient.ensureQueryData(query)));
+    }
   } catch (error) {
     if (error instanceof ApiError && error.status === 401) {
       setToken(null);
