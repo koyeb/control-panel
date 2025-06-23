9475ed0e7580d0944f59de1a525403b9c187c67d
nils <nilscox.dev@gmail.com>
2025-06-23 10:48:34 +0200

    add root page not found

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@koyeb/design-system';
 import { QueryClient } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
@@ -5,6 +6,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 import { PostHogProvider } from 'src/application/posthog';
 import { NotificationContainer } from 'src/components/notification';
+import { Translate } from 'src/intl/translate';
 
 type RouterContext = {
   queryClient: QueryClient;
@@ -13,6 +15,7 @@ type RouterContext = {
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: RootComponent,
+  notFoundComponent: PageNotFound,
 });
 
 function RootComponent() {
@@ -23,5 +26,26 @@ function RootComponent() {
       <TanStackRouterDevtools />
       <ReactQueryDevtools />
     </PostHogProvider>
+  );
+}
+
+function PageNotFound() {
+  return (
+    <div
+      className="col absolute inset-0 items-center justify-center gap-8 bg-black/95 bg-cover bg-center bg-no-repeat px-4 py-8 text-center text-white"
+      style={{ backgroundImage: 'url("/public/black-hole.svg")' }}
+    >
+      <div className="text-4xl md:text-6xl">
+        <Translate id="pages.notFound.title" />
+      </div>
+
+      <div className="text-base">
+        <Translate id="pages.notFound.line1" />
+      </div>
+
+      <Route.Link href="/" className={Button.className({ variant: 'ghost' }, 'mt-4')}>
+        <Translate id="pages.notFound.back" />
+      </Route.Link>
+    </div>
   );
 }
