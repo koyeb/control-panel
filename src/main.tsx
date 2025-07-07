723bdd5cd1583b5e6d4a4e746d47bd3163a57d27
nils <nilscox.dev@gmail.com>
2025-07-07 18:09:28 +0200

    parse search params with query-string

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles.css';
 
 import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
+import qs from 'query-string';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -120,6 +121,11 @@ const router = createRouter({
   defaultPendingMinMs: 0,
   defaultPreloadStaleTime: 0,
   scrollRestoration: true,
+  parseSearch: qs.parse,
+  stringifySearch: (value) => {
+    const result = qs.stringify(value);
+    return result !== '' ? `?${result}` : '';
+  },
   context: {
     queryClient,
     auth: undefined!,
