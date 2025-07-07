7c20e9998a0c8b8d94e5e25982ac94b4314deef1
nils <nilscox.dev@gmail.com>
2025-07-07 13:04:11 +0200

    handle account locked errors

--- src/routes/_main/route.tsx
+++ src/routes/_main/route.tsx
@@ -1,13 +1,12 @@
 import { Outlet, createFileRoute, redirect } from '@tanstack/react-router';
 
+import { isAccountLockedError } from 'src/api/api-errors';
+import { useOrganizationQuery, useUserQuery } from 'src/api/hooks/session';
 import { MainLayout } from 'src/layouts/main/main-layout';
+import { AccountLocked } from 'src/modules/account/account-locked';
 
 export const Route = createFileRoute('/_main')({
-  component: () => (
-    <MainLayout>
-      <Outlet />
-    </MainLayout>
-  ),
+  component: Component,
 
   beforeLoad({ location, context }) {
     const { auth } = context;
@@ -22,3 +21,24 @@ export const Route = createFileRoute('/_main')({
     }
   },
 });
+
+function Component() {
+  const userQuery = useUserQuery();
+  const organizationQuery = useOrganizationQuery();
+
+  const locked = [
+    isAccountLockedError(userQuery.error),
+    isAccountLockedError(organizationQuery.error),
+    organizationQuery.data?.statusMessage === 'VERIFICATION_FAILED',
+  ].some(Boolean);
+
+  if (locked) {
+    return <AccountLocked />;
+  }
+
+  return (
+    <MainLayout>
+      <Outlet />
+    </MainLayout>
+  );
+}
