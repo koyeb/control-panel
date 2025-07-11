589a4415269d9cd3f205453ce19d7887d6b41b38
nils <nilscox.dev@gmail.com>
2025-07-11 15:36:48 +0200

    remove the idenfy feature flag

--- src/application/idenfy.ts
+++ src/application/idenfy.ts
@@ -2,19 +2,17 @@ import { useQuery } from '@tanstack/react-query';
 
 import { useUserUnsafe } from 'src/api/hooks/session';
 import { useApiQueryFn } from 'src/api/use-api';
-import { useFeatureFlag } from 'src/hooks/feature-flag';
 
 export function useIdenfyLink() {
   const user = useUserUnsafe();
-  const hasIdenfy = useFeatureFlag('idenfy');
 
   const query = useQuery({
-    enabled: hasIdenfy && user !== undefined,
+    ...useApiQueryFn('getIdenfyToken'),
+    select: (result) => result.auth_token!,
+    enabled: user !== undefined,
     meta: { showError: false },
     refetchOnMount: false,
     refetchInterval: false,
-    ...useApiQueryFn('getIdenfyToken'),
-    select: (result) => result.auth_token!,
   });
 
   if (query.isSuccess) {
