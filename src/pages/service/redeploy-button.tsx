84334b51b1ad2cfb8143aa5b7ec2ba7931f67c7e
nils <nilscox.dev@gmail.com>
2025-07-22 11:16:28 +0200

    split redeploy button and dialog components

--- src/pages/service/redeploy-button.tsx
+++ src/pages/service/redeploy-button.tsx
@@ -21,6 +21,47 @@ import { createTranslate } from 'src/intl/translate';
 const T = createTranslate('pages.service.layout');
 
 export function RedeployButton({ app, service }: { app: App; service: Service }) {
+  const openDialog = Dialog.useOpen();
+  const [isServiceSettings] = useRoute(`/services/${service.id}/settings`);
+
+  if (isServiceSettings || service.status === 'PAUSED') {
+    return null;
+  }
+
+  return (
+    <>
+      <CliInfoButton
+        button={
+          <Button onClick={() => openDialog('Redeploy')} className="self-stretch sm:self-start">
+            <T id="redeploy" />
+          </Button>
+        }
+        tooltip={
+          <CliInfoTooltip
+            title={<T id="redeployCli.title" />}
+            description={<T id="redeployCli.description" />}
+            command={`koyeb service redeploy ${app.name}/${service.name}`}
+          />
+        }
+      />
+
+      <RedeployDialog id="Redeploy" service={service} />
+    </>
+  );
+}
+
+type RedeployDialogProps = {
+  id: string;
+  context?: Record<string, unknown>;
+  service: Service;
+};
+
+export function RedeployDialog({ id, context, service }: RedeployDialogProps) {
+  const t = T.useTranslate();
+  const closeDialog = Dialog.useClose();
+  const navigate = useNavigate();
+  const invalidate = useInvalidateApiQuery();
+
   const latestDeployment = useComputeDeployment(service.latestDeploymentId);
   const latestStashed = latestDeployment?.status === 'STASHED';
 
@@ -31,14 +72,6 @@ export function RedeployButton({ app, service }: { app: App; service: Service })
 
   const instance = useInstance(latestDeployment?.definition.instanceType);
 
-  const openDialog = Dialog.useOpen();
-  const closeDialog = Dialog.useClose();
-
-  const [isServiceSettings] = useRoute(`/services/${service.id}/settings`);
-  const navigate = useNavigate();
-
-  const t = T.useTranslate();
-
   const form = useForm({
     defaultValues: {
       skipBuild: false,
@@ -46,16 +79,11 @@ export function RedeployButton({ app, service }: { app: App; service: Service })
     },
   });
 
-  const invalidate = useInvalidateApiQuery();
-
-  const mutation = useMutation({
-    ...useApiMutationFn(
-      'redeployService',
-      ({ skipBuild: usePreviousBuild, useCache }: FormValues<typeof form>) => ({
-        path: { id: service.id },
-        body: { skip_build: usePreviousBuild, use_cache: useCache },
-      }),
-    ),
+  const redeploy = useMutation({
+    ...useApiMutationFn('redeployService', ({ skipBuild, useCache }: FormValues<typeof form>) => ({
+      path: { id: service.id },
+      body: { skip_build: skipBuild, use_cache: useCache },
+    })),
     async onSuccess({ deployment }) {
       await invalidate('listDeployments');
 
@@ -71,115 +99,93 @@ export function RedeployButton({ app, service }: { app: App; service: Service })
     },
   });
 
-  if (isServiceSettings || service.status === 'PAUSED') {
-    return null;
-  }
-
   const wasBuilt = hasBuild(latestDeployment) && service.lastProvisionedDeploymentId !== undefined;
 
   return (
-    <>
-      <CliInfoButton
-        button={
-          <Button
-            loading={form.formState.isSubmitting}
-            onClick={() => openDialog('Redeploy')}
-            className="self-stretch sm:self-start"
-          >
-            <T id="redeploy" />
-          </Button>
-        }
-        tooltip={
-          <CliInfoTooltip
-            title={<T id="redeployCli.title" />}
-            description={<T id="redeployCli.description" />}
-            command={`koyeb service redeploy ${app.name}/${service.name}`}
-          />
-        }
-      />
+    <Dialog id={id} context={context} onClosed={form.reset} className="col w-full max-w-xl gap-4">
+      <DialogHeader title={<T id="redeployDialog.title" />} />
 
-      <Dialog id="Redeploy" onClosed={form.reset} className="col w-full max-w-xl gap-4">
-        <DialogHeader title={<T id="redeployDialog.title" />} />
+      <p className="text-dim">
+        <T id={wasBuilt ? 'redeployDialog.descriptionWithBuild' : 'redeployDialog.descriptionWithoutBuild'} />
+      </p>
 
-        <p className="text-dim">
-          <T
-            id={wasBuilt ? 'redeployDialog.descriptionWithBuild' : 'redeployDialog.descriptionWithoutBuild'}
-          />
-        </p>
+      {availability === 'low' && (
+        <Alert
+          variant="info"
+          title={<T id="redeployDialog.lowCapacity.title" values={{ instance: instance?.displayName }} />}
+          description={<T id="redeployDialog.lowCapacity.description" />}
+        />
+      )}
 
-        {availability === 'low' && (
-          <Alert
-            variant="info"
-            title={<T id="redeployDialog.lowCapacity.title" values={{ instance: instance?.displayName }} />}
-            description={<T id="redeployDialog.lowCapacity.description" />}
-          />
+      <form onSubmit={handleSubmit(form, redeploy.mutateAsync)} className="col gap-2">
+        {wasBuilt && (
+          <>
+            <div className="col items-start gap-4 rounded-md border p-3">
+              <div className="col gap-1">
+                <div className="font-medium">
+                  <T id="redeployDialog.skipBuild.title" />
+                </div>
+
+                <div className="text-xs text-dim">
+                  <T id="redeployDialog.skipBuild.description" />
+                </div>
+              </div>
+
+              <Tooltip content={latestStashed && <T id="redeployDialog.skipBuild.latestStashed" />}>
+                {(props) => (
+                  <div {...props}>
+                    <Button
+                      type="submit"
+                      disabled={latestStashed}
+                      loading={form.formState.isSubmitting && form.watch('skipBuild')}
+                      onClick={() => {
+                        form.setValue('useCache', false);
+                        form.setValue('skipBuild', true);
+                      }}
+                    >
+                      <T id="redeployDialog.skipBuild.cta" />
+                      <IconArrowRight className="size-4" />
+                    </Button>
+                  </div>
+                )}
+              </Tooltip>
+            </div>
+
+            <div className="col items-start gap-4 rounded-md border p-3">
+              <div className="col gap-1">
+                <div className="font-medium">
+                  <T id="redeployDialog.triggerBuild.title" />
+                </div>
+
+                <div className="text-xs text-dim">
+                  <T id="redeployDialog.triggerBuild.description" />
+                </div>
+              </div>
+
+              <ControlledCheckbox
+                control={form.control}
+                name="useCache"
+                label={<T id="redeployDialog.triggerBuild.useCache" />}
+              />
+
+              <Button
+                type="submit"
+                loading={form.formState.isSubmitting && !form.watch('skipBuild')}
+                onClick={() => form.setValue('skipBuild', false)}
+              >
+                <T id="redeployDialog.triggerBuild.cta" />
+                <IconArrowRight className="size-4" />
+              </Button>
+            </div>
+          </>
         )}
 
-        <form onSubmit={handleSubmit(form, mutation.mutateAsync)} className="col gap-2">
-          {wasBuilt && (
-            <>
-              <div className="col items-start gap-4 rounded-md border p-3">
-                <div className="col gap-1">
-                  <div className="font-medium">
-                    <T id="redeployDialog.skipBuild.title" />
-                  </div>
-
-                  <div className="text-xs text-dim">
-                    <T id="redeployDialog.skipBuild.description" />
-                  </div>
-                </div>
-
-                <Tooltip content={latestStashed && <T id="redeployDialog.skipBuild.latestStashed" />}>
-                  {(props) => (
-                    <div {...props}>
-                      <Button
-                        type="submit"
-                        disabled={latestStashed}
-                        onClick={() => {
-                          form.setValue('useCache', false);
-                          form.setValue('skipBuild', true);
-                        }}
-                      >
-                        <T id="redeployDialog.skipBuild.cta" />
-                        <IconArrowRight className="size-4" />
-                      </Button>
-                    </div>
-                  )}
-                </Tooltip>
-              </div>
-
-              <div className="col items-start gap-4 rounded-md border p-3">
-                <div className="col gap-1">
-                  <div className="font-medium">
-                    <T id="redeployDialog.triggerBuild.title" />
-                  </div>
-
-                  <div className="text-xs text-dim">
-                    <T id="redeployDialog.triggerBuild.description" />
-                  </div>
-                </div>
-
-                <ControlledCheckbox
-                  control={form.control}
-                  name="useCache"
-                  label={<T id="redeployDialog.triggerBuild.useCache" />}
-                />
-
-                <Button type="submit" onClick={() => form.setValue('skipBuild', false)}>
-                  <T id="redeployDialog.triggerBuild.cta" />
-                  <IconArrowRight className="size-4" />
-                </Button>
-              </div>
-            </>
-          )}
-
-          {!wasBuilt && (
-            <Button type="submit" loading={form.formState.isSubmitting} autoFocus className="self-end">
-              <T id="redeployDialog.submitButton" />
-            </Button>
-          )}
-        </form>
-      </Dialog>
-    </>
+        {!wasBuilt && (
+          <Button type="submit" loading={form.formState.isSubmitting} autoFocus className="self-end">
+            <T id="redeployDialog.submitButton" />
+          </Button>
+        )}
+      </form>
+    </Dialog>
   );
 }
