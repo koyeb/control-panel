9c0612f4eb7db16c8e3244be049259e9597ffde1
nils <nilscox.dev@gmail.com>
2025-07-25 16:22:45 +0200

    refactor scaling rules logic

--- src/modules/service-form/helpers/scaling-rules.ts
+++ src/modules/service-form/helpers/scaling-rules.ts
@@ -11,59 +11,60 @@ type Target = keyof Scaling['targets'];
 export function useScalingRules() {
   const { getValues, setValue, resetField } = useFormContext<ServiceForm>();
 
+  const onScalingChanged = (min: number, max: number): void => {
+    const { serviceType, scaling } = getValues();
+
+    if (min > 0) {
+      setValue('scaling.scaleToZero.lightSleep.enabled', false, { shouldValidate: true });
+    }
+
+    if (min !== max && max > 1) {
+      const target: Target = serviceType === 'worker' ? 'cpu' : 'requests';
+      setValue(`scaling.targets.${target}.enabled`, true, { shouldValidate: true });
+    } else {
+      for (const target of keys(scaling.targets)) {
+        setValue(`scaling.targets.${target}.enabled`, false, { shouldValidate: true });
+        resetField(`scaling.targets.${target}.value`);
+      }
+    }
+  };
+
+  const onInstanceChanged = (previous: CatalogInstance | null, selected: CatalogInstance | null) => {
+    const { meta, scaling } = getValues();
+
+    const set = ({ min, max }: { min?: number; max?: number }) => {
+      if (min !== undefined) setValue('scaling.min', min, { shouldValidate: true });
+      if (max !== undefined) setValue('scaling.max', max, { shouldValidate: true });
+      onScalingChanged(min ?? scaling.min, max ?? scaling.max);
+    };
+
+    if (selected?.id === 'free') {
+      set({ min: 0, max: 1 });
+    }
+
+    if (selected?.category === 'eco' && selected?.id !== 'free') {
+      set({ min: scaling.max });
+    }
+
+    if (meta.serviceId === null && previous?.category !== 'gpu' && selected?.category === 'gpu') {
+      set({ min: 0 });
+    }
+
+    if (!isTenstorrentGpu(previous) && isTenstorrentGpu(selected)) {
+      set({ min: 1 });
+    }
+
+    if (isTenstorrentGpu(previous) && !isTenstorrentGpu(selected)) {
+      set({ min: 0 });
+    }
+
+    if (selected?.category === 'gpu') {
+      setValue('scaling.scaleToZero.lightSleep.enabled', false);
+    }
+  };
+
   return {
-    onScalingChanged(min: number, max: number) {
-      const { serviceType, scaling } = getValues();
-
-      if (min > 0) {
-        setValue('scaling.scaleToZero.lightSleep.enabled', false, { shouldValidate: true });
-      }
-
-      if (max > 1) {
-        const target: Target = serviceType === 'worker' ? 'cpu' : 'requests';
-        setValue(`scaling.targets.${target}.enabled`, true, { shouldValidate: true });
-      }
-
-      if (min === max || max === 1) {
-        for (const target of keys(scaling.targets)) {
-          setValue(`scaling.targets.${target}.enabled`, false, { shouldValidate: true });
-          resetField(`scaling.targets.${target}.value`);
-        }
-      }
-    },
-
-    onInstanceChanged(previous: CatalogInstance | null, selected: CatalogInstance | null) {
-      const { meta, scaling } = getValues();
-
-      if (selected?.id === 'free') {
-        setValue('scaling.min', 0, { shouldValidate: true });
-        setValue('scaling.max', 1, { shouldValidate: true });
-        this.onScalingChanged(0, 1);
-      }
-
-      if (selected?.category === 'eco' && selected?.id !== 'free') {
-        setValue('scaling.min', scaling.max, { shouldValidate: true });
-        this.onScalingChanged(scaling.max, scaling.max);
-      }
-
-      if (selected?.category === 'gpu') {
-        setValue('scaling.scaleToZero.lightSleep.enabled', false);
-      }
-
-      if (meta.serviceId === null && previous?.category !== 'gpu' && selected?.category === 'gpu') {
-        setValue('scaling.min', 0, { shouldValidate: true });
-        this.onScalingChanged(0, scaling.max);
-      }
-
-      if (!isTenstorrentGpu(previous) && isTenstorrentGpu(selected)) {
-        setValue('scaling.min', 1, { shouldValidate: true });
-        this.onScalingChanged(1, scaling.max);
-      }
-
-      if (isTenstorrentGpu(previous) && !isTenstorrentGpu(selected)) {
-        setValue('scaling.min', 0, { shouldValidate: true });
-        this.onScalingChanged(0, scaling.max);
-      }
-    },
+    onScalingChanged,
+    onInstanceChanged,
   };
 }
