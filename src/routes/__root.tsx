863345db25cacb9087c1203d1453a6c3e914692b
nils <nilscox.dev@gmail.com>
2025-07-28 13:21:54 +0200

    install tanstack router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,8 @@
+import { QueryClient } from '@tanstack/react-query';
+import { createRootRouteWithContext } from '@tanstack/react-router';
+
+type RouterContext = {
+  queryClient: QueryClient;
+};
+
+export const Route = createRootRouteWithContext<RouterContext>()({});
