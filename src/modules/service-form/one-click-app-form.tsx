64bb1a099a87c735b7b563afde1f986fafd8c187
nils <nilscox.dev@gmail.com>
2025-08-26 15:16:24 +0200

    handle volume creation with one-click apps

--- src/modules/service-form/one-click-app-form.tsx
+++ src/modules/service-form/one-click-app-form.tsx
@@ -75,9 +75,18 @@ export function OneClickAppForm({ app, onCostChanged }: OneClickAppFormProps) {
 
   const serviceForm = useMemo(() => {
     return merge(
-      defaultServiceForm(),
-      deploymentDefinitionToServiceForm(app.definition, githubApp?.organizationName, []),
-      { meta: { appId }, appName: app.slug },
+      { ...defaultServiceForm(), environmentVariables: [] },
+      deploymentDefinitionToServiceForm(app.deploymentDefinition, githubApp?.organizationName, []),
+      {
+        meta: { appId },
+        appName: app.slug,
+        volumes: app.templateDefinition?.volumes?.map((volume) => ({
+          name: volume.name,
+          size: volume.size,
+          mountPath: volume.path,
+          mounted: false,
+        })),
+      },
     );
   }, [app, githubApp, appId]);
 
@@ -91,13 +100,7 @@ export function OneClickAppForm({ app, onCostChanged }: OneClickAppFormProps) {
         regions: [],
       })),
     },
-    resolver: useZodResolver(
-      z.object({
-        instance: z.string(),
-        regions: z.array(z.string()).min(1),
-        environmentVariables: z.array(createEnvironmentVariableSchema(app.templateEnv)),
-      }),
-    ),
+    resolver: useZodResolver(createSchema(app)),
   });
 
   const mutation = useMutation({
@@ -145,6 +148,14 @@ export function OneClickAppForm({ app, onCostChanged }: OneClickAppFormProps) {
       <ServiceFormUpgradeDialog plan={requiredPlan} submitForm={() => formRef.current?.requestSubmit()} />
     </FormProvider>
   );
+}
+
+function createSchema(app: OneClickApp): z.ZodType<OneClickAppForm> {
+  return z.object({
+    instance: z.string(),
+    regions: z.array(z.string()).min(1),
+    environmentVariables: z.array(createEnvironmentVariableSchema(app.templateEnv)),
+  });
 }
 
 function createEnvironmentVariableSchema(env: OneClickAppEnv[]): z.ZodType<EnvironmentVariable> {
