32cffaad77062263674dea7048aac2d50a54f45e
nils <nilscox.dev@gmail.com>
2025-10-10 16:42:49 +0200

    move the deployment metadata next to the deployment info component

--- src/modules/deployment/deployment-info/deployment-info.tsx
+++ src/modules/deployment/deployment-info/deployment-info.tsx
@@ -1,19 +1,19 @@
 import { useDeploymentScaling } from 'src/api';
+import { useVolumes } from 'src/api';
 import { openDialog } from 'src/components/dialog';
+import { ExternalLink } from 'src/components/link';
+import { Metadata } from 'src/components/metadata';
 import { ServiceTypeIcon } from 'src/components/service-type-icon';
+import { Tooltip } from 'src/components/tooltip';
+import { IconDocker, IconGitBranch, IconGitCommitHorizontal, IconGithub } from 'src/icons';
 import { TranslateEnum, createTranslate } from 'src/intl/translate';
 import { App, ComputeDeployment, Service } from 'src/model';
+import { DeploymentDefinition, EnvironmentVariable } from 'src/model';
+import { assert } from 'src/utils/assert';
+import { hasProperty } from 'src/utils/object';
+import { shortId } from 'src/utils/strings';
 
 import { InstanceMetadata, RegionsMetadata, ScalingMetadata } from '../metadata';
-import {
-  BranchMetadata,
-  BuilderMetadata,
-  CommitMetadata,
-  PrivilegedMetadata,
-  RepositoryMetadata,
-} from '../metadata/build-metadata';
-import { DockerImageMetadata } from '../metadata/docker-metadata';
-import { EnvironmentMetadata, VolumesMetadata } from '../metadata/runtime-metadata';
 
 import { DeploymentDefinitionDialog } from './deployment-definition-dialog';
 import { ExternalUrl } from './external-url';
@@ -94,5 +94,213 @@ export function DeploymentInfo({ app, service, deployment }: DeploymentInfoProps
 
       <DeploymentDefinitionDialog />
     </section>
+  );
+}
+
+export function DockerImageMetadata({ image }: { image: string }) {
+  return (
+    <Metadata
+      label={<T id="dockerImageLabel" />}
+      value={
+        <div className="row items-center gap-2">
+          <div>
+            <IconDocker className="size-em" />
+          </div>
+          <div>{image}</div>
+        </div>
+      }
+    />
+  );
+}
+
+export function RepositoryMetadata({ repository }: { repository: string | null }) {
+  return (
+    <Metadata
+      label={<T id="repositoryLabel" />}
+      value={
+        <div className="row items-center gap-2">
+          <span>
+            <IconGithub className="size-em" />
+          </span>
+
+          {repository === null && '-'}
+
+          {repository !== null && (
+            <ExternalLink openInNewTab href={`https://${repository}`}>
+              {repository.replace(/^github.com\//, '')}
+            </ExternalLink>
+          )}
+        </div>
+      }
+    />
+  );
+}
+
+export function BranchMetadata({ repository, branch }: { repository: string | null; branch: string | null }) {
+  return (
+    <Metadata
+      label={<T id="branchLabel" />}
+      value={
+        <div className="row items-center gap-2">
+          <span>
+            <IconGitBranch className="size-em" />
+          </span>
+
+          {repository !== null || (branch !== null && '-')}
+
+          {repository !== null && branch !== null && (
+            <ExternalLink openInNewTab href={`https://${repository}/tree/${branch}`}>
+              {branch}
+            </ExternalLink>
+          )}
+        </div>
+      }
+    />
+  );
+}
+
+export function CommitMetadata({ deployment }: { deployment: ComputeDeployment }) {
+  const source = deployment.definition.source;
+  const sha = shortId(deployment.build?.sha);
+
+  assert(source.type === 'git');
+
+  if (sha === undefined) {
+    return null;
+  }
+
+  return (
+    <Metadata
+      label={<T id="commitLabel" />}
+      value={
+        <div className="row items-center gap-2">
+          <span>
+            <IconGitCommitHorizontal className="size-em" />
+          </span>
+
+          <ExternalLink href={`https://${source.repository}/commit/${sha}`}>{sha}</ExternalLink>
+        </div>
+      }
+    />
+  );
+}
+
+export function BuilderMetadata({ builder }: { builder: DeploymentDefinition['builder'] }) {
+  assert(builder !== undefined);
+
+  return (
+    <Metadata
+      label={<T id="builderLabel" />}
+      value={
+        <div className="row items-center gap-2">
+          <T id={builder.type} />
+        </div>
+      }
+    />
+  );
+}
+
+export function PrivilegedMetadata({ privileged }: { privileged?: boolean }) {
+  assert(privileged !== undefined);
+
+  return (
+    <Metadata
+      label={<T id="privilegedLabel" />}
+      value={
+        <div className="row items-center gap-2">
+          <T id={String(privileged) as 'true' | 'false'} />
+        </div>
+      }
+    />
+  );
+}
+
+export function EnvironmentMetadata({ definition }: { definition: DeploymentDefinition }) {
+  const { environmentVariables, files } = definition;
+
+  const content = () => {
+    if (environmentVariables.length === 0) {
+      return null;
+    }
+
+    return (
+      <div className="whitespace-nowrap">
+        {environmentVariables.map(formatEnvironmentVariable).map((line, index) => (
+          <div key={index} className="truncate">
+            {line}
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <Metadata
+      label={<T id="environmentLabel" />}
+      value={
+        <Tooltip
+          allowHover
+          content={content()}
+          className="max-w-md"
+          trigger={(props) => (
+            <span {...props}>
+              <T
+                id="environmentValue"
+                values={{ variables: environmentVariables.length, files: files.length }}
+              />
+            </span>
+          )}
+        />
+      }
+    />
+  );
+}
+
+function formatEnvironmentVariable({ name, value }: EnvironmentVariable) {
+  return `${name}=${value}`;
+}
+
+export function VolumesMetadata({ definition }: { definition: DeploymentDefinition }) {
+  const { volumes: attachedVolumes } = definition;
+  const volumes = useVolumes();
+
+  const content = () => {
+    if (attachedVolumes.length === 0) {
+      return null;
+    }
+
+    return (
+      <div className="whitespace-nowrap">
+        {attachedVolumes.map(({ volumeId, mountPath }) => (
+          <div key={volumeId} className="truncate">
+            <T
+              id="attachedVolume"
+              values={{
+                volumeName: volumes?.find(hasProperty('id', volumeId))?.name,
+                mountPath,
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <Metadata
+      label={<T id="volumesLabel" />}
+      value={
+        <Tooltip
+          allowHover
+          content={content()}
+          className="max-w-md"
+          trigger={(props) => (
+            <span {...props}>
+              <T id="volumesValue" values={{ count: attachedVolumes.length }} />
+            </span>
+          )}
+        />
+      }
+    />
   );
 }
