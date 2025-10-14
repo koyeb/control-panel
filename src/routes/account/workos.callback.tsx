86b7cb25a90c761bbb33c1186589fab44416d91e
nils <nilscox.dev@gmail.com>
2025-10-14 15:32:32 +0200

    wait for the user to be created when signing up with workos

--- src/routes/account/workos.callback.tsx
+++ src/routes/account/workos.callback.tsx
@@ -1,8 +1,11 @@
+import { QueryClient } from '@tanstack/react-query';
 import { createFileRoute, redirect } from '@tanstack/react-router';
 import { z } from 'zod';
 
+import { ApiError, apiQuery } from 'src/api';
 import { LogoLoading } from 'src/components/logo-loading';
 import { urlToLinkOptions } from 'src/hooks/router';
+import { wait } from 'src/utils/promises';
 
 export const Route = createFileRoute('/account/workos/callback')({
   pendingComponent: LogoLoading,
@@ -14,7 +17,19 @@ export const Route = createFileRoute('/account/workos/callback')({
     state: z.string().optional(),
   }),
 
-  async loader({ context: { authKit } }) {
+  async loader({ context: { queryClient, authKit } }) {
+    await waitForUser(queryClient);
     throw redirect(urlToLinkOptions(authKit.next ?? '/'));
   },
 });
+
+async function waitForUser(queryClient: QueryClient) {
+  try {
+    await queryClient.ensureQueryData(apiQuery('get /v1/account/profile', {}));
+  } catch (error) {
+    if (ApiError.is(error) && error.message === 'User id is not a uuid: ""') {
+      await wait(1000);
+      return waitForUser(queryClient);
+    }
+  }
+}
