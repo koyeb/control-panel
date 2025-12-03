2bec2cac1000ad2619c7517c7d18893236702a4a
nils <nilscox.dev@gmail.com>
2025-12-03 20:31:20 +0100

    restore the session token logic

--- src/application/token.ts
+++ src/application/token.ts
@@ -1,11 +1,20 @@
 type GetAccessToken = () => Promise<string>;
 
 let _getAccessToken: GetAccessToken;
+let sessionToken: string | null = null;
 
 export function setGetAccessToken(getAccessToken: GetAccessToken) {
   _getAccessToken = () => getAccessToken();
 }
 
+export function setSessionToken(token: string) {
+  sessionToken = token;
+}
+
 export function getToken() {
+  if (sessionToken) {
+    return Promise.resolve(sessionToken);
+  }
+
   return _getAccessToken();
 }
