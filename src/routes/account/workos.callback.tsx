18340c10e0a4f70e1181c6a533f207176fa115cf
nils <nilscox.dev@gmail.com>
2025-12-09 17:19:54 +0100

    remove the fallback to redirect from the workos callback page

--- src/routes/account/workos.callback.tsx
+++ src/routes/account/workos.callback.tsx
@@ -1,25 +1,15 @@
-import { Navigate, createFileRoute } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 import { z } from 'zod';
 
 import { LogoLoading } from 'src/components/logo-loading';
-import { wait } from 'src/utils/promises';
 
 export const Route = createFileRoute('/account/workos/callback')({
   pendingComponent: LogoLoading,
   pendingMinMs: 0,
   pendingMs: 0,
 
-  component() {
-    return <Navigate to="/" />;
-  },
-
   validateSearch: z.object({
     code: z.string().optional(),
     state: z.string().optional(),
   }),
-
-  async loader() {
-    // authkit should redirect from onRedirectCallback
-    await wait(5_000);
-  },
 });
