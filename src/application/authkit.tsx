98c12896e5460ada951948959cc494a872179933
nils <nilscox.dev@gmail.com>
2025-12-17 15:57:46 +0100

    simplify authentication error handling

--- src/application/authkit.tsx
+++ src/application/authkit.tsx
@@ -1,5 +1,5 @@
 import { QueryClient } from '@tanstack/react-query';
-import { NavigateFn } from '@tanstack/react-router';
+import { RegisteredRouter } from '@tanstack/react-router';
 import {
   AuthKitProvider as BaseAuthKitProvider,
   LoginRequiredError,
@@ -8,6 +8,7 @@ import {
 import { useLayoutEffect, useState } from 'react';
 
 import { ApiError, apiQuery } from 'src/api';
+import { ErrorComponent } from 'src/components/error-view';
 import { LogoLoading } from 'src/components/logo-loading';
 import { urlToLinkOptions } from 'src/hooks/router';
 import { assert } from 'src/utils/assert';
@@ -25,15 +26,12 @@ declare global {
 globalThis._getAccessToken = () => Promise.resolve(null);
 
 type AuthKitProviderProps = {
-  navigate: NavigateFn;
+  router: RegisteredRouter;
   queryClient: QueryClient;
+  children: (authKit: AuthKit) => React.ReactNode;
 };
 
-export function AuthKitProvider({
-  navigate,
-  queryClient,
-  ...props
-}: AuthKitProviderProps & AuthKitGuardProps) {
+export function AuthKitProvider({ router, queryClient, children }: AuthKitProviderProps) {
   const clientId = getConfig('workOsClientId');
   const apiHostname = getConfig('workOsApiHost');
   const environment = getConfig('environment');
@@ -44,7 +42,7 @@ export function AuthKitProvider({
 
   const onRedirectCallback = async (next?: string) => {
     await waitForUser(queryClient);
-    await navigate(urlToLinkOptions(next ?? '/'));
+    await router.navigate(urlToLinkOptions(next ?? '/'));
   };
 
   return (
@@ -57,38 +55,59 @@ export function AuthKitProvider({
       onBeforeAutoRefresh={() => true}
       onRefresh={() => setKey(key + 1)}
     >
-      <AuthKitGuard key={key} {...props} />
+      <AuthKitGuard key={key} router={router}>
+        {children}
+      </AuthKitGuard>
     </BaseAuthKitProvider>
   );
 }
 
 type AuthKitGuardProps = {
-  onLoginRequired: () => Promise<void>;
+  router: RegisteredRouter;
   children: (authKit: AuthKit) => React.ReactNode;
 };
 
-function AuthKitGuard({ onLoginRequired, children }: AuthKitGuardProps) {
+function AuthKitGuard({ router, children }: AuthKitGuardProps) {
+  const [error, setError] = useState<unknown>(null);
   const authKit = useAuth();
 
   useLayoutEffect(() => {
     const onError = async (error: unknown) => {
       if (error instanceof LoginRequiredError) {
-        await onLoginRequired();
-        return null;
+        await handleLoginRequiredError(router);
       } else {
-        throw error;
+        setError(error);
       }
+
+      return null;
     };
 
     globalThis._getAccessToken = () => authKit.getAccessToken().catch(onError);
     (globalThis as { authKit?: AuthKit }).authKit = authKit;
   });
 
+  if (error instanceof Error) {
+    return <ErrorComponent error={error} reset={() => setError(null)} />;
+  }
+
   if (authKit.isLoading) {
     return <LogoLoading />;
   }
 
   return children(authKit);
+}
+
+async function handleLoginRequiredError(router: RegisteredRouter) {
+  const { pathname, searchStr } = router.latestLocation;
+
+  if (!pathname.startsWith('/auth') && !pathname.startsWith('/account')) {
+    const next = pathname + searchStr;
+
+    await router.navigate({
+      to: '/auth/signin',
+      search: next === '/' ? undefined : { next },
+    });
+  }
 }
 
 // eslint-disable-next-line react-refresh/only-export-components
