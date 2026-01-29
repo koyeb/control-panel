e3ac46be486f503cb50f5794603c75497b6235b8
nils <nilscox.dev@gmail.com>
2026-01-29 10:58:23 +0100

    dedup toasts from text content

--- src/application/notify.ts
+++ src/application/notify.ts
@@ -1,6 +1,9 @@
 import { toast } from 'react-toastify';
+import z from 'zod';
 
 import { Toast } from 'src/components/notification';
+
+import { createValidationGuard } from './validation';
 
 type NotifyOptions = {
   title?: React.ReactNode;
@@ -21,15 +24,46 @@ export const notify: NotifierPort = {
   error: createNotify('error'),
 };
 
+const toasts = new Set<string>();
+
 function createNotify(variant: 'success' | 'info' | 'warning' | 'error') {
-  return (text: React.ReactNode, { title, ...options }: NotifyOptions = {}): void => {
+  return (children: React.ReactNode, { title, autoClose }: NotifyOptions = {}): void => {
+    const text = getNodeText(children);
+
+    if (toasts.has(text)) {
+      return;
+    }
+
+    toasts.add(text);
+
     toast(Toast, {
-      ...options,
+      onClose: () => toasts.delete(text),
+      autoClose,
       data: {
         variant,
         title,
-        children: text,
+        children,
       },
     });
   };
 }
+
+function getNodeText(node: React.ReactNode): string {
+  if (typeof node === 'string' || typeof node === 'number') {
+    return String(node);
+  }
+
+  if (Array.isArray(node)) {
+    return node.map(getNodeText).join('');
+  }
+
+  if (hasChildrenProp(node)) {
+    return getNodeText(node.props.children);
+  }
+
+  return '';
+}
+
+const hasChildrenProp = createValidationGuard(
+  z.object({ props: z.object({ children: z.custom<React.ReactNode>() }) }),
+);
