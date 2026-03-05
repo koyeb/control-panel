7bb1337967d925a4d4fc666ddd2939fe088d408c
nils <nilscox.dev@gmail.com>
2026-03-05 18:39:26 +0100

    allow to change the default project from the organization settings page

--- src/pages/settings/organization/general/general-settings.page.tsx
+++ src/pages/settings/organization/general/general-settings.page.tsx
@@ -1,16 +1,23 @@
 import { useOrganization } from 'src/api';
+import { FeatureFlag } from 'src/hooks/feature-flag';
 import { DeactivateOrganization } from 'src/modules/account/deactivate-organization';
 import { DeleteOrganization } from 'src/modules/account/delete-organization';
 import { ReactivateOrganization } from 'src/modules/account/reactivate-organization';
 
+import { DefaultProject } from './default-project';
 import { OrganizationName } from './organization-name';
 import { OrganizationQuotas } from './organization-quotas';
 
 export function GeneralSettingsPage() {
+  const organization = useOrganization();
+
   return (
     <>
       <OrganizationName />
       <OrganizationQuotas />
+      <FeatureFlag feature="simple-projects">
+        <DefaultProject key={organization?.id} />
+      </FeatureFlag>
       <DangerZone />
     </>
   );
