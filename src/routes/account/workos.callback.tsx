88a4325e3daf2b026bbb658d40f94ac0c7fa0c9d
nils <nilscox.dev@gmail.com>
2026-03-09 10:36:16 +0100

    clear the current project id when logging in

--- src/routes/account/workos.callback.tsx
+++ src/routes/account/workos.callback.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { z } from 'zod';
 
+import { setCurrentProjectId } from 'src/api/hooks/project';
+
 export const Route = createFileRoute('/account/workos/callback')({
   validateSearch: z.object({
     code: z.string().optional(),
     state: z.string().optional(),
   }),
+
+  beforeLoad() {
+    setCurrentProjectId(null);
+  },
 });
